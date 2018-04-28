e796afacf8ebe4d47a04b081a6a2eb6dce174ee1
harry12800 <804151219@qq.com>
2018-04-28 09:19:14 +0800

    第一次提交

---
+++ src/cn/harry12800/lnk/diary/SearchResultPanel.java
@@ -0,0 +1,13 @@
+package cn.harry12800.lnk.diary;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class SearchResultPanel extends JPanel {
+
+	public SearchResultPanel() {
+		setLayout(null);
+		setOpaque(false);
+		setVisible(false);
+	}
+}
